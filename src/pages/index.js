733634c0178b8db0c9ee9ae1bd1003f1c18ec139
aerykpayne <hello@aerykpayne.com>
2018-02-22 16:21:14 -0500

    splash page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,36 @@
-import React from "react";
-import Link from "gatsby-link";
-import Script from "react-load-script";
-import graphql from "graphql";
+import React from "react"
+import Link from "gatsby-link"
+import Script from "react-load-script"
+import graphql from "graphql"
+import nicer from "../img/nicer.png"
+import rocket from "../img/rocket.png"
+import { css } from "emotion"
+
+const className = css`
+  background-color: #10112f;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  padding-left: 200px;
+  h3 {
+    margin-top: 40px;
+    font-weight: bold;
+    font-size: 22px;
+    color: white;
+  }
+`
+const anotherClassName = css`
+  width: 800px;
+`
+const rocketClass = css`
+  position: absolute;
+  width: 2250px;
+  left: -40%;
+  bottom: -40%;
+`
 
 export default class IndexPage extends React.Component {
   handleScriptLoad() {
@@ -9,56 +38,64 @@ export default class IndexPage extends React.Component {
       window.netlifyIdentity.on("init", user => {
         if (!user) {
           window.netlifyIdentity.on("login", () => {
-            document.location.href = "/admin/";
-          });
+            document.location.href = "/admin/"
+          })
         }
-      });
+      })
     }
-    window.netlifyIdentity.init();
+    window.netlifyIdentity.init()
   }
 
   render() {
-    const { data } = this.props;
-    const { edges: posts } = data.allMarkdownRemark;
+    const { data } = this.props
+    const { edges: posts } = data.allMarkdownRemark
 
     return (
-      <section className="section">
-        <Script
-          url="https://identity.netlify.com/v1/netlify-identity-widget.js"
-          onLoad={() => this.handleScriptLoad()}
-        />
-        <div className="container">
-          <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
-          </div>
-          {posts
-            .filter(post => post.node.frontmatter.templateKey === "blog-post")
-            .map(({ node: post }) => (
-              <div
-                className="content"
-                style={{ border: "1px solid #eaecee", padding: "2em 4em" }}
-                key={post.id}
-              >
-                <p>
-                  <Link className="has-text-primary" to={post.frontmatter.path}>
-                    {post.frontmatter.title}
-                  </Link>
-                  <span> &bull; </span>
-                  <small>{post.frontmatter.date}</small>
-                </p>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button is-small" to={post.frontmatter.path}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </div>
-            ))}
+      <div className={className}>
+        <img className={anotherClassName} src={nicer} />
+        <h3>We build and launch brands, websites, and apps.</h3>
+        <div className={rocketClass}>
+          <img src={rocket} />
         </div>
-      </section>
-    );
+      </div>
+
+      // <section className="section">
+      //   <Script
+      //     url="https://identity.netlify.com/v1/netlify-identity-widget.js"
+      //     onLoad={() => this.handleScriptLoad()}
+      //   />
+      //   <div className="container">
+      //     <div className="content">
+      //       <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+      //     </div>
+      //     {posts
+      //       .filter(post => post.node.frontmatter.templateKey === "blog-post")
+      //       .map(({ node: post }) => (
+      //         <div
+      //           className="content"
+      //           style={{ border: "1px solid #eaecee", padding: "2em 4em" }}
+      //           key={post.id}
+      //         >
+      //           <p>
+      //             <Link className="has-text-primary" to={post.frontmatter.path}>
+      //               {post.frontmatter.title}
+      //             </Link>
+      //             <span> &bull; </span>
+      //             <small>{post.frontmatter.date}</small>
+      //           </p>
+      //           <p>
+      //             {post.excerpt}
+      //             <br />
+      //             <br />
+      //             <Link className="button is-small" to={post.frontmatter.path}>
+      //               Keep Reading →
+      //             </Link>
+      //           </p>
+      //         </div>
+      //       ))}
+      //   </div>
+      // </section>
+    )
   }
 }
 
@@ -79,4 +116,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
+`
